1c5144a9cbdb9b81e5ee7be46b77add156074e4d
Hybson Maia <hybsonfmaia@hotmail.com>
2022-11-23 16:40:30 -0300

    host

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express()
 
 app.set('port', process.env.PORT || 5000)
 
-app.use(express.static(__dirname))
+app.use(express.static('webapp'))
 
 //server starting
 var server = app.listen(app.get('port'), function () {
